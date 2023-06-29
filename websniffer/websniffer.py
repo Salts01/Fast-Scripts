5ce7f719779a412224a2c90f444c6a0037ef2e6f
Salts01 <gui.rnb@gmail.com>
2023-06-29 15:17:04 -0300

    atualização 1

--- websniffer/websniffer.py
+++ websniffer/websniffer.py
@@ -1,5 +1,10 @@
 import requests
 
+print("______Bem vindo o websniffer!_______")
+print("-------------------------------------------------")
+print("Não utilize essa ferramenta para fins maliciosos")
+print("-------------------------------------------------")
+
 url=input("digite a Url: ")
 lista=input("digite a lista a ser usada: ")
 with open(lista,'r') as words:
@@ -11,5 +16,21 @@ with open(lista,'r') as words:
 			if response.status_code==200:
 				print("")
 				print("-------------------------------")
-				print("encontrado","["+word+"]")
+				print("encontrado "+"/"+word)
 				print("-------------------------------")
+		for word in words:
+			response=requests.get(url+'/'+word+"."+ext)
+			if response.status_code==200:
+				print("")
+				print("-------------------------------")
+				print("encontrado "+"/"+word+"."+ext)
+				print("-------------------------------")
+	else:
+		for word in words:
+			response=requests.get(url+'/'+word)
+			if response.status_code==200:
+				print("")
+				print("-------------------------------")
+				print("encontrado "+"/"+word)
+				print("-------------------------------")
+				
